9697dbcb01d33a714ed199253b2bc56e382355a2
Yangying <ryysophie@126.com>
2023-03-05 22:36:41 -0800

    Update Slack Content

--- My Last Day/Assets/Scripts/Zlack/ZlackInteraction.cs
+++ My Last Day/Assets/Scripts/Zlack/ZlackInteraction.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ZlackInteraction : MonoBehaviour
 {
-    public GameObject MemberList;
+    public GameObject MemberList, chooseChannel;
+    private GameObject chosenChannel;
+    public Transform Channels;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +16,8 @@ public class ZlackInteraction : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+   
     }
     public void CheckMemberList(){
     MemberList.SetActive(true);
@@ -22,4 +25,11 @@ public class ZlackInteraction : MonoBehaviour
     public void UnCheckMemberList(){
     MemberList.SetActive(false);
     }
+
+    public void ChooseChannel(){
+    if(Channels.childCount!=0){
+     Destroy(Channels.GetChild(0).gameObject);
+    }
+    chosenChannel=Instantiate(chooseChannel,Channels);
+     chosenChannel.transform.SetParent(Channels);    }
 }
